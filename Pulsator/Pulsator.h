5fb384a75a66c5854e862b50b57c224f7346d787
shu223 <shuichi0526@gmail.com>
2016-04-12 06:22:38 +0900

    Add Carthage support

---
+++ Pulsator/Pulsator.h
@@ -0,0 +1,19 @@
+//
+//  Pulsator.h
+//  Pulsator
+//
+//  Created by Shuichi Tsutsumi on 4/12/16.
+//  Copyright © 2016 Shuichi Tsutsumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pulsator.
+FOUNDATION_EXPORT double PulsatorVersionNumber;
+
+//! Project version string for Pulsator.
+FOUNDATION_EXPORT const unsigned char PulsatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pulsator/PublicHeader.h>
+
+
