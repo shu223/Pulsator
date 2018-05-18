81ce5c51f4c339271f513fb541da0e31f1d2bf16
shu223 <shuichi0526@gmail.com>
2018-05-17 19:05:11 -0700

    Fixed a Carthage build error

---
+++ Pulsator-macOS/Pulsator_macOS.h
@@ -0,0 +1,19 @@
+//
+//  Pulsator_macOS.h
+//  Pulsator-macOS
+//
+//  Created by Shuichi Tsutsumi on 2018/05/17.
+//  Copyright © 2018 Shuichi Tsutsumi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Pulsator_macOS.
+FOUNDATION_EXPORT double Pulsator_macOSVersionNumber;
+
+//! Project version string for Pulsator_macOS.
+FOUNDATION_EXPORT const unsigned char Pulsator_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pulsator_macOS/PublicHeader.h>
+
+
---
+++ PulsatorMac/PulsatorMac.h
@@ -0,0 +1,19 @@
+//
+//  PulsatorMac.h
+//  PulsatorMac
+//
+//  Created by Shuichi Tsutsumi on 2018/05/17.
+//  Copyright © 2018 Shuichi Tsutsumi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PulsatorMac.
+FOUNDATION_EXPORT double PulsatorMacVersionNumber;
+
+//! Project version string for PulsatorMac.
+FOUNDATION_EXPORT const unsigned char PulsatorMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PulsatorMac/PublicHeader.h>
+
+
