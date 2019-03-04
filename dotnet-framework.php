e9ef91622f7aaf4d2cd5ecd1d266ccde96ac636f
Oscar Gomez Elastic-Aluminum-Levitated-API <42280574+oscarg933@users.noreply.github.com>
2019-03-04 08:42:39 -0700

    Create dotnet-framework.php

---
+++ dotnet-framework.php
@@ -0,0 +1,118 @@
+Skip to content
+Your account has been flagged.
+Because of that, your profile is hidden from the public. If you believe this is a mistake, contact support to have your account status reviewed.
+bluetooth pulse
+Repositories49
+Code1M
+Commits4K
+Issues1K
+Marketplace0
+Topics0
+Wikis381
+Users0
+Language
+
+Sort
+
+49 repository results
+shu223/Pulsator
+Pulse animation for iOS
+pulse-animation
+map
+swift
+ios
+animation
+bluetooth
+bluetooth-low-energy
+ibeacon
+MIT license
+
+Updated 9 days ago
+
+ Swift
+ 1k
+EHfive/pulseaudio-modules-bt
+Adds Sony LDAC, aptX, aptX HD, AAC codecs (A2DP Audio) support to PulseAudio on Linux
+audio
+linux
+aac
+bluetooth
+a2dp
+aptx
+GPL-3.0 license
+
+Updated 4 days ago
+
+ C
+ 347
+openhomeautomation/bluetooth-pulse-rate-sensor
+Code for the Bluetooth Heart-Rate Sensor with Arduino project
+Updated on Aug 30, 2016
+
+ CSS
+ 1
+kvaps/docker-pulseaudio-bluetooth Archived
+Bluez5 and PulseAudio in a docker for recieve and send sound via a2dp
+MIT license
+
+Updated on Jan 2, 2016
+
+ Shell
+ 5
+NextDom/plugin-PulseAudio
+Plugin Jeedom/NextDom permettant la gestion du son via et vers des PC, enceintes Bluetooth, enceintes USB, sorties j…
+GPL-2.0 license
+
+Updated on Jul 13, 2018
+
+ PHP
+ 1
+albertcbraun/CMS50FW
+A library for conveniently connecting over Bluetooth to a Contec Medical Systems Pulse Oximeter (model CMS50FW) and r…
+Updated on Oct 5, 2015
+
+ Java
+ 14
+vats28/Contec-CMS50EW
+A sample project to show communication methodology for contec Bluetooth pulse oximeter.
+Updated on Apr 28, 2015
+
+ Java
+ 2
+eatnumber1/bluez-pulse-loader
+Loads a loopback module into pulseaudio when a bluetooth client connects.
+Updated on Apr 20, 2012
+
+ Ruby
+ 4
+woahbase/alpine-pulseaudio
+Pulseaudio Network Sound Server + Bluez Bluetooth Daemon built on Alpine Linux + S6
+pulseaudio
+alpine
+bluetooth
+bluez
+docker
+sound
+server
+Updated on Jan 13
+
+ Makefile
+ 3
+jmmcmillan/Nonin9560
+Driver for Nonin 9560 bluetooth pulse oximeter
+Updated on Feb 4, 2013
+
+ C#
+ 2
+© 2019 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
