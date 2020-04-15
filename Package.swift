d62f630f85b8bcab80d1d1db854ead40836cc9b9
shu223 <shuichi0526@gmail.com>
2020-04-15 14:47:23 +0900

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Pulsator",
+    platforms: [.iOS(.v8)],
+    products: [
+        .library(
+            name: "Pulsator",
+            targets: ["Pulsator"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "Pulsator",
+            dependencies: [],
+            path: "Pulsator"),
+        .testTarget(
+            name: "PulsatorTests",
+            dependencies: ["Pulsator"]),
+    ]
+)
